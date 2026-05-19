93459ba32996f1798d78c2c52a841b412c859e84
shibili jobe <shibilijob@gmail.com>
2026-05-19 17:55:32 +0530

    Time: 1 ms (11.02%), Space: 54.6 MB (9.84%) - LeetHub

---
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var subtractProductAndSum = function(n) {
+    let product = 1;
+    let sum = 0;
+    while (n > 0) {
+        let digit = n % 10;
+        product *= digit;
+        sum += digit;
+        n = Math.floor(n / 10);
+    }
+
+    return product - sum;
+};
