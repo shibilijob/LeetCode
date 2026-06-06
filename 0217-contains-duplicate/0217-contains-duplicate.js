81541a2b5bfe968badde2d731e90ba789eee1241
shibili jobe <shibilijob@gmail.com>
2026-06-06 11:23:29 +0530

    Time: 73 ms (8.08%), Space: 64 MB (99.78%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -4,6 +4,7 @@
  */
 var containsDuplicate = function(nums) {
     let sorted = nums.sort((a,b)=>a-b)
+    
     for(let i=0;i<sorted.length-1;i++){
         if(sorted[i]===sorted[i+1]){
             return true
