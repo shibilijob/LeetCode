e83e6d160d91ad4c95ba3211f6b30b852a6ad605
shibili jobe <shibilijob@gmail.com>
2026-07-07 11:23:35 +0530

    Time: 2 ms (9.9%), Space: 54.2 MB (84.39%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -8,9 +8,10 @@ var searchRange = function(nums, target) {
     let last = -1
     for(let i=0;i<nums.length;i++){
         if(nums[i]===target){
-            if(first===-1)
+            if(first===-1){
                 first = i;
-                last=i
+            }
+            last=i
         }
     }
     return [first,last]
