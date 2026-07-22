18584359b952c35fb66fe88e9f5517377393e939
shibili jobe <shibilijob@gmail.com>
2026-07-22 16:03:40 +0530

    Time: 1 ms (73.33%), Space: 56 MB (76%) - LeetHub

---
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var removeTrailingZeros = function(num) {
+    let i = num.length-1;
+    while(num[i]==0){
+        i--
+    }
+    return num.substring(0,i+1)
+};
